4b94b350710f5280c57fc09c4382be1457e51732
amanda_s <129100961+amandastat@users.noreply.github.com>
2023-03-27 20:47:40 +0200

    Update rtweet_base.R

--- rtweet_base.R
+++ rtweet_base.R
@@ -1,11 +1,11 @@
 library(rtweet)
 
 token <- create_token(
-  app = "tesi_di_laurea",
+  app = "",
   consumer_key = "K0Ry8umWeuoEYfdlljIJpiiCk",
   consumer_secret = "MNhf3RqxOqnpxQionU6bYrYpDa8mMstQGzXePDcO3DTwESqVEq",
-  access_token = "1246618694941396997-JbLewQkAtVIqIwYE8SnsxVWPTxM0KV",
-  access_secret = "Cr7YKjF38KDzhGK9FRLcZmw3KWDvRSbgGks8rGUGlATfQ")
+  access_token = "",
+  access_secret = "")
 rt <- search_tweets(
   "Scudetto", n = 18000, include_rts = FALSE, token = token
 )
